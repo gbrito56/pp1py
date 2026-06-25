ea5cb0df3c879fdb82db2d01edd3d357e063d3c4
gbrito56 <gabrito56@gmail.com>
2026-06-25 19:54:42 -0300

    Update articulos.py

--- tp/1/src/schemas/articulos.py
+++ tp/1/src/schemas/articulos.py
@@ -8,18 +8,18 @@ IntPrecioVenta = Annotated[int, Field(ge=500, lt=999999)]
 BoolActivo = Annotated[bool, Field(description="Sigue disponible?")]
 IdBuscado = Annotated[int, Path(gt=0)]
 
-class ArticuloSchema(BaseModel):
+class ArticuloSchema(BaseModel): #modelo de articulo
     id: Annotated[int, Field(gt=0, description="ID del articulo", deprecated=True)]
     nombre: StrCortito
     precio: IntPrecioVenta = 1500
     activo: BoolActivo = True
 
-class ArticuloNuevoSchema(BaseModel):
+class ArticuloNuevoSchema(BaseModel): #modelo de articulo nuevo
     nombre: StrCortito
     precio: IntPrecioVenta
     activo: BoolActivo = True
 
-class ArticuloUpdateSchema(BaseModel):
+class ArticuloUpdateSchema(BaseModel): #modelo de edicion de articulo
     nombre: StrCortito
     precio: IntPrecioVenta = 2000
     activo: BoolActivo = True
